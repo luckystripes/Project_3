8f196c7fc832d97f2258d375e0862c13544cd31a
Austin Langley <austinwlangley@gmail.com>
2015-05-10 10:28:01 -0700

    added seeker controller test for index and show actions

--- spec/controllers/seekers_controller_spec.rb
+++ spec/controllers/seekers_controller_spec.rb
@@ -2,4 +2,39 @@ require 'rails_helper'
 
 RSpec.describe SeekersController, type: :controller do
 
-end
+#-------------------------------------------------------------------------------
+	describe "GET #index" do
+	    before :each do
+	    get :index
+	    end
+	    it "should render the correct page" do
+	      expect(response).to render_template("index")
+	    end
+	    it "have status code of 200" do 
+	      expect(response).to have_http_status(200)
+	    end
+	end
+#-------------------------------------------------------------------------------
+	describe "GET #show" do
+	    before :each do
+	    @seeker1 = Seeker.create(:first_name "John", :last_name "Thomas", :email "John@example.com", :phone"555-555-5555", :job_type"painter")
+	    get :show, id: @seeker1
+	    end
+	    it "should render the correct page" do
+	      expect(response).to render_template("show")
+	    end
+	    it "have status code of 200" do 
+	      expect(response).to have_http_status(200)
+	    end
+	    it "assigns @seeker to include seeker" do
+	      expect(assigns(:seeker)).to eq(@seeker)
+	    end
+	end
+#-------------------------------------------------------------------------------
+
+
+
+
+
+
+end  #end of RSpec
