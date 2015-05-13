cc5e7317d58d0464dabbd4fa359c8ac10d8b6365
Austin Langley <austinwlangley@gmail.com>
2015-05-13 16:49:46 -0700

    added has_attached_file to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   has_secure_password
 # attr_reader :password
 
