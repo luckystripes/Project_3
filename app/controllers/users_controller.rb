21c93c71906d1391076b430211d2f92355a606ee
Austin Langley <austinwlangley@gmail.com>
2015-05-17 15:34:09 -0700

    added index action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
  
+ def index
+ 	respond_to do |format|
+        format.html { render 'front' }
+        format.json { render json: Job.all[1..9]}
+    end
+ end
+
 end
