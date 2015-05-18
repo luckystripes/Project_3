79e995d623fadd2c5b062a7a63be631f82c4559e
Nancy Ma <luckystripesbiz@gmail.com>
2015-05-18 09:15:05 -0700

    paperclip setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514001515) do
+ActiveRecord::Schema.define(version: 20150518154840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
