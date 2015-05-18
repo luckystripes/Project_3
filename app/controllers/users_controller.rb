0c0288acdf9646e32b7880d72bd781ec99644aea
Austin Langley <austinwlangley@gmail.com>
2015-05-17 18:15:06 -0700

    added background image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,11 @@ class UsersController < ApplicationController
  	respond_to do |format|
         format.html { render 'index' }
         format.json { render json: Job.all[1..9]}
+        format.json { render json: Match.all}
     end
  end
+
+
+
 
 end
